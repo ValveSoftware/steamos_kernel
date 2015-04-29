3e6434d87226dd01f8ca4e9e1c4a61094f1a167a
Adam D. Moss <c@yotes.com>
2015-04-29 16:30:25 +0100

    xpad: fix race condition / usb whine in force feedback code
    Was re-using an urb without acquiring the lock.
    
    Should fix kernel warnings of this nature:
    "URB ffff88020d047d80 submitted while active"

--- drivers/input/joystick/xpad.c
+++ drivers/input/joystick/xpad.c
@@ -897,11 +897,14 @@ static void xpad_deinit_output(struct usb_xpad *xpad)
 #ifdef CONFIG_JOYSTICK_XPAD_FF
 static int xpad_play_effect(struct input_dev *dev, void *data, struct ff_effect *effect)
 {
+	int retval = 0;
 	struct usb_xpad *xpad = input_get_drvdata(dev);
 
 	if (effect->type == FF_RUMBLE) {
 		__u16 strong = effect->u.rumble.strong_magnitude;
 		__u16 weak = effect->u.rumble.weak_magnitude;
+
+		spin_lock(&xpad->odata_lock);
 
 		switch (xpad->xtype) {
 
@@ -914,7 +917,8 @@ static int xpad_play_effect(struct input_dev *dev, void *data, struct ff_effect 
 			xpad->odata[5] = weak / 256;	/* right actuator */
 			xpad->irq_out->transfer_buffer_length = 6;
 
-			return usb_submit_urb(xpad->irq_out, GFP_ATOMIC);
+			retval = usb_submit_urb(xpad->irq_out, GFP_ATOMIC);
+			break;
 
 		case XTYPE_XBOX360:
 			xpad->odata[0] = 0x00;
@@ -927,7 +931,8 @@ static int xpad_play_effect(struct input_dev *dev, void *data, struct ff_effect 
 			xpad->odata[7] = 0x00;
 			xpad->irq_out->transfer_buffer_length = 8;
 
-			return usb_submit_urb(xpad->irq_out, GFP_ATOMIC);
+			retval = usb_submit_urb(xpad->irq_out, GFP_ATOMIC);
+			break;
 
 		case XTYPE_XBOX360W:
 			xpad->odata[0] = 0x00;
@@ -944,7 +949,8 @@ static int xpad_play_effect(struct input_dev *dev, void *data, struct ff_effect 
 			xpad->odata[11] = 0x00;
 			xpad->irq_out->transfer_buffer_length = 12;
 
-			return usb_submit_urb(xpad->irq_out, GFP_ATOMIC);
+			retval = usb_submit_urb(xpad->irq_out, GFP_ATOMIC);
+			break;
 
 		case XTYPE_XBOXONE:
 			xpad->odata[0] = 0x09;
@@ -962,17 +968,21 @@ static int xpad_play_effect(struct input_dev *dev, void *data, struct ff_effect 
 			xpad->odata[12] = 0x00;
 			xpad->irq_out->transfer_buffer_length = 13;
 
-			return usb_submit_urb(xpad->irq_out, GFP_ATOMIC);
+			retval = usb_submit_urb(xpad->irq_out, GFP_ATOMIC);
+			break;
 
 		default:
 			dev_dbg(&xpad->dev->dev,
 				"%s - rumble command sent to unsupported xpad type: %d\n",
 				__func__, xpad->xtype);
-			return -1;
+			retval = -1;
+			break;
 		}
+
+		spin_unlock(&xpad->odata_lock);
 	}
 
-	return 0;
+	return retval;
 }
 
 static int xpad_init_ff(struct usb_xpad *xpad)
